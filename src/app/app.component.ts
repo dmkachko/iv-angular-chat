71ef88bc53a91e4a1fe934c0d3c032eb5fc1e50b
Dmitriy Kachko <dmkachko@gmail.com>
2022-05-08 21:49:45 -0400

    Add socket-io

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {io} from 'socket.io-client';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'chat';
+  ngOnInit(): void {
+    const key = 'a427604465';
+    const BASE_URL = `https://my-app-52ehg.ondigitalocean.app`;
+    const socket = io(BASE_URL);
+    socket.emit('events', { key });
+    socket.on('events', (v) => console.log('new messages', v));
+  }
 }
